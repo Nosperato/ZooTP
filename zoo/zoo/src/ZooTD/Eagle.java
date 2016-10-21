e31d5eb3001a833628e899fcb86751ff366f2a32
MadBlitz <thibaultdu84@live.fr>
2016-10-21 13:34:39 +0200

    Add files via upload

---
+++ zoo/zoo/src/ZooTD/Eagle.java
@@ -0,0 +1,7 @@
+package ZooTD;
+
+/**
+ * Created by d13002386 on 10/10/2016.
+ */
+public class Eagle {
+}
---
+++ zoo/zoo/src/ZooTD/MammalAction.java
@@ -0,0 +1,9 @@
+package ZooTD;
+
+/**
+ * Created by d13002386 on 10/10/2016.
+ */
+public interface MammalAction {
+
+    public Mammal birth();
+}
